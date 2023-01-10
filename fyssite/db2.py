7300031af9faa9151967d5ad0a1600ea0156e9d9
Maxl1911 <31564861+Maxl1911@users.noreply.github.com>
2023-01-10 15:34:59 +0100

    Update db2.py

--- fyssite/db2.py
+++ fyssite/db2.py
@@ -8,12 +8,15 @@ from __init__ import *
 
 def db(surname):
     ## makes an variable with connection information of the database.
-    connection = mariadb.connect(user="corendon", password='corendon', host="127.0.0.1", database="Corendon")
+    connection = mariadb.connect(user="corendon", password='corendon', host="127.0.0.1", database="Co>
 
     print(connection) ## Pint the status of the connection
     mycursor = connection.cursor()
-    mycursor.execute("USE Corendon;")
-    mycursor.execute(f"SELECT ticketnummer FROM Passagier WHERE achternaam = '{surname}';")
+    # mycursor.execute("USE Corendon;")
+    # mycursor.execute("SELECT ticketnummer FROM Passagier WHERE achternaam = %s'", (surname, ))
+    mycursor.execute("SELECT ticketnummer FROM Passagier WHERE achternaam = '%s'", (surname, ));
+    mycursor.execute("SELECT ticketnummer FROM Passagier WHERE achternaam = %(surname)s", {'surname':>
+
     result = mycursor.fetchone()
     data = str(result[0])
     print(data)
